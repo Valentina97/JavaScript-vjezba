4d209f88d48de584cb87fd6eae9bbe026715da13
Valentina97 <43275092+Valentina97@users.noreply.github.com>
2018-09-14 23:40:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+ //zadaca
+  
+ var Ime = prompt ("Dobrodošao! Kako se zoveš?");
+  alert("Bok " + Ime + " nadam se da si dobro. U nastavku Vas očekuje par dosadnih pitanja, pripremi mozak!");
+ 
+ var Pitanje = prompt ("Ukoliko uzmeš dvije jabuke od tri, koliko ih imaš?");
+if  ( Pitanje == 2) 
+{
+	
+	alert ("Bravo,nastavi tako dalje."); 
+}
+else
+{
+	
+alert("Pokušaj ponovno."); 
+	
+	
+}
+
+ var drugo = prompt ("Za skuhati jedno jaje treba 8 minuta. Koliko vremena treba da se skuhaju 3 jaja?");
+if  ( drugo == 8) 
+{
+	
+	alert ("Bravo,nastavi tako dalje."); 
+}
+else
+{
+	
+alert("Pokušaj ponovno."); 
+	
+	
+}
+
+ var trece = prompt ("Koji broj slijedi u nizu : 1 17 24 45 50 .. ?");
+if  ( trece == 79) 
+{
+	
+	alert ("Bravo,nastavi tako dalje."); 
+}
+else
+{
+	
+alert("Pokušaj ponovno."); 
+	
+	
+}	
